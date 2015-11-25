00f7734f88ea314f923696af518aee58177ae7dc
Daniel <daniel65536@gmail.com>
2015-11-25 18:15:35 +0800

    hyphen tags and categories

--- index.js
+++ index.js
@@ -2,13 +2,30 @@ var Hypher = require('hypher'),
     english = require('hyphenation.en-us'),
     h = new Hypher(english);
 
-function camel_case(m) {
-  return m.replace(/([a-z])([A-Z])/g, "$1\u00AD$2");
+function hyphenate(m) {
+  return h.hyphenateText(m.replace(/([a-z])([A-Z])/g, "$1\u00AD$2"));
 }
 
+function meta_hyphen(m) {
+  m.data = m.data.map(function(d) {
+      d.name = hyphenate(d.name)
+      return d
+  });
+}
+
+hexo.extend.filter.register('template_locals', function(locals){
+  if (locals.page.categories) {
+    meta_hyphen(locals.page.categories)
+  }
+  if (locals.page.tags) {
+    meta_hyphen(locals.page.tags)
+  }
+  return locals;
+});
+
 hexo.extend.filter.register('before_post_render', function(data) {
-  data.title = h.hyphenateText(camel_case(data.title));
-  data.content = h.hyphenateText(camel_case(data.content));
+  data.title = hyphenate(data.title);
+  data.content = hyphenate(data.content);
 });
 
 function remove_hyphen(m) {
@@ -25,4 +42,3 @@ hexo.extend.filter.register('after_post_render', function(data) {
   data.title = after_hyphen(data.title);
   data.content = after_hyphen(data.content);
 });
-
